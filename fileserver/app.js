b74e6220ab62015f4094183d42373be954aedc7d
Nicholas Young <nicholas@nicholaswyoung.com>
2015-02-20 15:53:47 -0600

    Add streams fileserver.

---
+++ fileserver/app.js
@@ -0,0 +1,10 @@
+var path = require('path')
+,   fs   = require('fs')
+,   app  = require('express')();
+
+app.get('/', function (req, res) {
+  res.type('html');
+  fs.createReadStream('./index.html').pipe(res);
+});
+
+app.listen(9292);
